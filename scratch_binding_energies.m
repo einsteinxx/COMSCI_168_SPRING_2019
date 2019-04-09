3520f6b6ecfa9cfb1b299cc943a274a380369873
K <keane.gonzalez@gmail.com>
2019-04-08 18:25:21 -0700

    Add files via upload
    scratch work for going through xray and photon notes

---
+++ scratch_binding_energies.m
@@ -0,0 +1,17 @@
+
+
+Z =1;
+for Z = 1:120
+    for ii = 1:7
+        e(Z,ii) = -13.6 *Z^2/ii^2;
+    end
+
+    
+end
+
+%%
+close all
+    figure, plot((-1)*e(1:80,1)/1000.0,'ro-');
+    grid on;
+    title('K SHELLS');
+    %matches plots of K shells for first 80 elements 
